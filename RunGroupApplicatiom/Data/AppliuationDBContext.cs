d6bec9f43a40839f1d89c8345531d521a37afa8f
typar <typarris421@gmail.com>
2022-05-18 18:51:30 -0400

    Add project files.

---
+++ RunGroupApplicatiom/Data/AppliuationDBContext.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using RunGroupApplicatiom.Models;
+
+namespace RunGroupApplicatiom.Data
+{
+    public class AppliuationDBContext :DbContext
+    {
+        public AppliuationDBContext(DbContextOptions<AppliuationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Races> Racess { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
+
+// Entity Framework
+// SQL Server
---
+++ RunGroupApplicatiom/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿namespace RunGroupApplicatiom.Models
+{
+    public class AppUser 
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+
+        public Address? Address { get; set; }
+        
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Races> Races { get; set; }
+    }
+}
